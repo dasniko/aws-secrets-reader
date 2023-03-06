196e3697cc903c0a28c9d1765ca275c28d4d889d
Niko Köbler <niko@n-k.de>
2023-03-06 08:08:01 +0100

    printable version and deps updated

--- src/main/java/dasniko/aws/secretsmanager/AwsSecretsReader.java
+++ src/main/java/dasniko/aws/secretsmanager/AwsSecretsReader.java
@@ -9,12 +9,26 @@ import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 public class AwsSecretsReader {
 
     public static void main(String[] args) {
-        SecretsManagerClient secretsManager = SecretsManagerClient.builder()
+        if (args.length > 1) {
+            throw new RuntimeException("Wrong number of arguments provided, only one argument allowed.");
+        }
+        String arg = args[0];
+        if (arg.equals("--version") || arg.equals("-v")) {
+            System.out.println("AWS Secrets Reader by @dasniko, version 1.2");
+        } else {
+            readSecret(arg);
+        }
+    }
+
+    private static void readSecret(String secretName) {
+        String secretString;
+        try (SecretsManagerClient secretsManager = SecretsManagerClient.builder()
             .httpClientBuilder(UrlConnectionHttpClient.builder())
-            .build();
-        String secretString = secretsManager
-            .getSecretValue(builder -> builder.secretId(args[0]).build())
-            .secretString();
+            .build()) {
+            secretString = secretsManager
+                .getSecretValue(builder -> builder.secretId(secretName).build())
+                .secretString();
+        }
         System.out.println(secretString);
     }
 
